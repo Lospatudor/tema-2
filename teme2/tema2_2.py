82f4650b13a5c7a679351ad5ad61c12b68f2aee5
Lospatudor <87440661+Lospatudor@users.noreply.github.com>
2021-12-06 17:01:31 +0100

    Update tema2_2.py

--- teme2/tema2_2.py
+++ teme2/tema2_2.py
@@ -1,33 +1,33 @@
 #Sum numbers from 0 to n
-#def sum_num(n):
-#    if n == 0:
-#        return 0
-#    return n + sum_num(n-1)
-#
-#
-#print(sum_num(5))
+def sum_num(n):
+    if n == 0:
+        return 0
+    return n + sum_num(n-1)
+
+
+print(sum_num(5))
 
 #Sum even numbers from 0 to n
-#def sum_even(n):
-#    if n == 0:
-#        return 0
-#    if n % 2 == 0:
-#        return n + sum_even(n - 1)
-#    else:
-#        return sum_even(n - 1)
-#
-#
-#print(sum_even(5))
+def sum_even(n):
+    if n == 0:
+        return 0
+    if n % 2 == 0:
+        return n + sum_even(n - 1)
+    else:
+        return sum_even(n - 1)
+
+
+print(sum_even(5))
 
 
 #Sum odd numbers from 0 to n
-#def sum_even(n):
-#    if n == 0:
-#        return 0
-#    if n % 2 == 1:
-#        return n + sum_even(n - 1)
-#    else:
-#        return sum_even(n - 1)
-#
-#
-#print(sum_even(7))
+def sum_even(n):
+    if n == 0:
+        return 0
+    if n % 2 == 1:
+        return n + sum_even(n - 1)
+    else:
+        return sum_even(n - 1)
+
+
+print(sum_even(7))
